22671ac304a40ba7d502e6848578872ff3e0959d
MuhIlham420 <muh.ilhammbo@gmail.com>
2026-01-26 21:56:52 +0700

    menambahkan struktur react dan beberapa file

---
+++ client/src/features/auth/pages/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <div className="">
+        <button
+          className="border-3"
+          onClick={() => setCount((count) => count + 1)}
+        >
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="">Click on the Vite and React logos to learn more</p>
+    </>
+  );
+}
+
+export default App;
